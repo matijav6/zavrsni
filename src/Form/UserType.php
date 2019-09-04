0677a19bb07c9014321424299a6256532e0cfd70
Matija Vuk <mvuk27@gmail.com>
2019-09-04 16:40:21 +0200

    user crud before visual

---
+++ src/Form/UserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Colleges;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+//            ->add('roles', ChoiceType::class, [
+//                'choices' => [
+//                    'admin' =>'ROLE_ADMIN',
+//                    'user' => 'ROLE_USER'
+//                ]
+//            ])
+            ->add('password')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('colleges', EntityType::class, [
+                'class' => Colleges::class,
+                'choice_label' => 'name',
+                'multiple' => true
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
