33ba41a1550f980f1e7e32f73056813104443795
Matija Vuk <mvuk27@gmail.com>
2019-09-07 15:11:45 +0200

    users can view posts

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,21 +3,68 @@
 
 namespace App\Controller;
 
+use App\Entity\Posts;
+use App\Entity\PostTypes;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/user")
+ * @Route("/user/dashboard")
  */
 class UserController extends AbstractController
 {
     /**
      * @return Response
-     * @Route(path="/dashboard", name="user.dashboard")
+     * @Route(path="/news", name="user.news")
      */
-    public function getDashboard()
+    public function getNews()
     {
-        return $this->render('user/dashboard.html.twig');
+        $type = $this->getDoctrine()->getRepository(PostTypes::class)->findOneBy([
+            'name' => "Novosti"
+        ]);
+        $posts = $this->getDoctrine()->getRepository(Posts::class)->findBy([
+            'type' => $type
+        ]);
+        return $this->render('user/dashboard.html.twig', [
+            'posts' => $posts,
+            'file' => false
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @Route(path="/instructions", name="user.instructions")
+     */
+    public function getInstructions()
+    {
+        $type = $this->getDoctrine()->getRepository(PostTypes::class)->findOneBy([
+            'name' => "Instrukcije"
+        ]);
+        $posts = $this->getDoctrine()->getRepository(Posts::class)->findBy([
+            'type' => $type
+        ]);
+        return $this->render('user/dashboard.html.twig', [
+            'posts' => $posts,
+            'file' => false
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @Route(path="/materials", name="user.materials")
+     */
+    public function getMaterials()
+    {
+        $type = $this->getDoctrine()->getRepository(PostTypes::class)->findOneBy([
+            'name' => "Materijali"
+        ]);
+        $posts = $this->getDoctrine()->getRepository(Posts::class)->findBy([
+            'type' => $type
+        ]);
+        return $this->render('user/dashboard.html.twig', [
+            'posts' => $posts,
+            'file' => true
+        ]);
     }
 }
